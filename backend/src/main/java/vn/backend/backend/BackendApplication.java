2691ae8f133a1be826fe6184e7d0011e3f4f789b
QuangHai115NB <quanghaidg@gmail.com>
2025-12-24 12:08:10 +0700

    demo

---
+++ backend/src/main/java/vn/backend/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package backend.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendShareApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/QuangHai/BillShare/BillShareApplicationTests.java
@@ -0,0 +1,13 @@
+package QuangHai.BillShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
